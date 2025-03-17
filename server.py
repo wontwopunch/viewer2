ee245859eaac8f36b7f3c17a75bb0474d58aebd1
wontwopunch <jiwontwopunch@gmail.com>
2025-03-17 13:39:49 +0900

    db 생성

--- server.py
+++ server.py
@@ -485,4 +485,4 @@ def get_public_tile(filename, level, x, y):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
